42b0f1d5c5324861fa4596b117996b9d0a454c21
Salah Al-Dhaferi <binaryminded@gmail.com>
2023-03-25 17:17:55 +0300

    feat(navbar): create the Navbar component along with it's test

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,5 @@
+const Navbar = () => {
+    return <div>Nsavbar</div>;
+};
+
+export default Navbar;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,12 +4,14 @@ import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import * as React from "react";
 
 import Layout from "@/layout/Layout";
+import Navbar from "@/components/Navbar";
 
 export default function HomePage() {
     const { t } = useTranslation("common");
 
     return (
         <Layout>
+            <Navbar />
             <p>{t("test")}</p>
             <div style={{ display: "flex", flexDirection: "row", gap: "20px" }}>
                 <Link href='/' locale='en'>
